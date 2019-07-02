fc3ce5e2f379fc1f7bc74c64dcd8f30e5043d202
Alexander G. Yang <spark8402@rogers.com>
2019-07-01 21:50:26 -0400

    assignment done

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-function arrayToObj() {
+function arrayToObj(arr) {
    // your code goes here  
-}
+
+   return arr.reduce((a, [b, c]) => (a[b] = c, a), {});
+
+    
+};
+
 
 module.exports = arrayToObj;
